bc4ad61315bb4f1edbb5a9fc608f94bfb785f049
exarkun0x00 <hi.fane@appstoreafrica.com>
2021-01-29 15:53:02 +0000

    kickoff app

--- App.tsx
+++ App.tsx
@@ -5,7 +5,16 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+};
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
@@ -15,8 +24,10 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
+            <PaperProvider theme={theme}>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
+        </PaperProvider>
       </SafeAreaProvider>
     );
   }
